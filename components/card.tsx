0e51cfa894bb2d44cb790272139e1b8b394c5b9e
AbdalmajeeDW <abd.almajed.alhashemi@gmail.com>
2025-03-21 13:34:44 +0200

    fixed card

--- components/card.tsx
+++ components/card.tsx
@@ -36,14 +36,13 @@ console.log(count);
   };
 
   return (
-    <div className="md:grid md:grid-cols-2 xl:grid xl:grid-cols-4 lg:grid xs:w-full lg:grid-cols-3 sm:grid sm:grid-cols-2 sm:gap-14 xs:grid xs:justify-items-center xs:grid-cols-1 gap-4 px-10 my-5 dark:bg-darkPrimary">
+    <div className="md:grid md:grid-cols-2 xl:grid xl:grid-cols-4 lg:grid xs:w-full lg:grid-cols-3 sm:grid sm:grid-cols-1 sm:gap-14 xs:grid xs:justify-items-center xs:grid-cols-1 gap-4 px-10 my-5 dark:bg-darkPrimary">
       {count.map(
         (e, i) =>
           (e.categorie === categorie || categorie === "home") && (
             <div
-            style={{width:'18.5rem'}}
               key={i}
-              className=" bg-white h-full  py-2 shadow-xl rounded-2xl shadow-shadowCard transition duration-300 dark:bg-darkSecondary dark:text-darkFontHero"
+              className=" bg-white h-full sm:w-80 xs:w-80 md:w-72 lg:w-72 xl:w-72   py-2 shadow-xl rounded-2xl shadow-shadowCard transition duration-300 dark:bg-darkSecondary dark:text-darkFontHero"
             >
               <div className="">
 
@@ -57,12 +56,13 @@ console.log(count);
               </div>
               <div className="flex justify-between flex-col">
                 <div className="px-4 py-2  text-titelCard flex  items-center dark:text-darkText">
-                  <div className="text-nowrap font-bold  text-xl">{e.title}</div>
-                  <div className="px-1 flex gap-1  text-gray font-bold dark:text-darkText">
+                  <div className="text-nowrap font-bold  text-xl">{e.title}
+                    
+                  </div>
+                  <div className="px-1 flex gap-1  text-gray  dark:text-darkText">
                     {e.price} <span className="text-shadowCard">ل.س</span>
                   </div>
                 </div>
-                <div className="px-6 flex text-lg text-graydark:text-darkGray"></div>{" "}
                 {e.additions && e.additions.length > 0 && (
                   <div className="px-4 mt-2">
                     {/* <h3 className="text-lg font-semibold text-gray-700 dark:text-darkText">الإضافات:</h3> */}
@@ -71,7 +71,7 @@ console.log(count);
                         key={index}
                         className="text-lg text-gray dark:text-darkText mb-1 flex gap-1"
                       >
-                        <span className="text-nowrap">{addition.title}</span>:{" "}
+                        <span className="">{addition.title}</span>:{" "}
                         {addition.price}{" "}
                         <span className="text-shadowCard">ل.س</span>
                       </div>
