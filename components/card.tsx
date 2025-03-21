fa641d2adecdea0a38167f0643bd50d9a179da1e
AbdalmajeeDW <abd.almajed.alhashemi@gmail.com>
2025-03-21 13:18:59 +0200

    fixed size product card

--- components/card.tsx
+++ components/card.tsx
@@ -41,14 +41,15 @@ console.log(count);
         (e, i) =>
           (e.categorie === categorie || categorie === "home") && (
             <div
+            style={{width:'18.5rem'}}
               key={i}
-              className="relative bg-white h-full w-72  py-2 shadow-xl rounded-2xl shadow-shadowCard transition duration-300 dark:bg-darkSecondary dark:text-darkFontHero"
+              className=" bg-white h-full  py-2 shadow-xl rounded-2xl shadow-shadowCard transition duration-300 dark:bg-darkSecondary dark:text-darkFontHero"
             >
               <div className="">
 
               <Image
                 src={e.product.src}
-                className={`${e.product.src.includes('/logo_MrCoffe2.png')?`h-52 w-44`:`h-72`} w-full p-1 py-0 rounded-2xl object-fill`}
+                className={`h-72 w-full p-1 py-0 rounded-2xl object-fill`}
                 width={300}
                 height={50}
                 alt={e.title}
@@ -57,7 +58,7 @@ console.log(count);
               <div className="flex justify-between flex-col">
                 <div className="px-4 py-2  text-titelCard flex  items-center dark:text-darkText">
                   <div className="text-nowrap font-bold  text-xl">{e.title}</div>
-                  <div className="px-2 flex gap-1  text-gray font-bold dark:text-darkText">
+                  <div className="px-1 flex gap-1  text-gray font-bold dark:text-darkText">
                     {e.price} <span className="text-shadowCard">ل.س</span>
                   </div>
                 </div>
