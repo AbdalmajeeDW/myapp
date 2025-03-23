634a8df22fda5603ce0427c51f67a29b60339788
AbdalmajeeDW <abd.almajed.alhashemi@gmail.com>
2025-03-23 17:08:45 +0200

    fixed null img

--- components/card.tsx
+++ components/card.tsx
@@ -8,8 +8,8 @@ interface Product {
   title: string;
   Description: string;
   price: string;
-  width?:string;
-  height?:string;
+  width?: string;
+  height?: string;
   additions?: { title: string; price: string }[];
   subCategorie?: string;
   product: { src: string };
@@ -30,34 +30,39 @@ export default function Home({ categories }: CardProps) {
 
   return (
     <div className="md:grid md:grid-cols-2 xl:grid xl:grid-cols-4 lg:grid xs:w-full lg:grid-cols-3 sm:grid sm:grid-cols-1 sm:gap-14 xs:grid xs:justify-items-center xs:grid-cols-1 gap-4 px-10 my-5 dark:bg-darkPrimary">
-      {count.filter((product) =>
-        product.categorie === categorie ||
-        product.subCategorie === categorie ||
-        categorie === "home"
-      ).map(
-        (e, i) =>
-        (
+      {count
+        .filter(
+          (product) =>
+            product.categorie === categorie ||
+            product.subCategorie === categorie ||
+            categorie === "home"
+        )
+        .map((e, i) => (
           <div
             key={i}
             className=" bg-white h-full sm:w-80 xs:w-80 md:w-72 lg:w-72 xl:w-72   py-2 shadow-xl rounded-2xl shadow-shadowCard transition duration-300 dark:bg-darkSecondary dark:text-darkFontHero"
           >
             <div className=" h-96 flex items-center justify-center">
-
               <Image
-                src={e.product?.src }
+                src={e.product.src}
                 className={`${
                   e.height ? `h-auto` : `h-96`
-                } p-1 w-full h-96 object-cover rounded-2xl`}  width={2239}
-  height={2799}
+                } p-1 w-full h-96 object-cover rounded-2xl`}
+                width={2239}
+                height={2799}
                 alt={e.title}
               />
             </div>
             <div className="">
-              <div style={{ display: 'flex', alignItems: 'center' }} className="px-4 py-2  text-titelCard dark:text-darkText">
-                <div className="text-nowrap font-bold font-hk  text-xl">{e.title}
-
-                </div>
-                <div style={{ display: 'flex', alignItems: 'center' }} className="px-1 font-hk flex gap-1  text-gray items-center font-bold  dark:text-darkText">
+              <div
+                style={{ display: "flex", alignItems: "center" }}
+                className="px-4 py-2  text-titelCard dark:text-darkText"
+              >
+                <div className="text-nowrap font-bold font-hk  ">{e.title}</div>
+                <div
+                  style={{ display: "flex", alignItems: "center" }}
+                  className="px-1 font-hk flex gap-1  text-gray items-center font-bold  dark:text-darkText"
+                >
                   {e.price} <span className="text-shadowCard font-hk">ل.س</span>
                 </div>
               </div>
@@ -80,8 +85,7 @@ export default function Home({ categories }: CardProps) {
               </div>
             </div>
           </div>
-        )
-      )}
+        ))}
     </div>
   );
 }
